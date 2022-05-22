c5712d0671fe6031165e4fe4e8af0328c694a32f
Bekzat <bekzat.bagdatuly03@gmail.com>
2022-05-22 16:23:27 +0600

    spotify

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen flex justify-center items-center">
+      <h1 className="text-7xl font-black">
+        Spotify Clone
+      </h1>
     </div>
   );
 }
