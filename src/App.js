78514dbfe8bb684d81518cbbf2017d0b928340f9
Bekzat <bekzat.bagdatuly03@gmail.com>
2022-06-09 21:20:45 +0600

    spotify

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import TheSidebar from './TheSidebar'
+import TheSidebarOverlay from './TheSidebarOverlay'
+import TheHeader from './TheHeader'
+import TheMain from './TheMain'
+import TheRegistration from './TheRegistration'
+
 function App() {
   return (
-    <div className="h-screen flex justify-center items-center">
-      <h1 className="text-7xl font-black">
-        Spotify Clone
-      </h1>
-    </div>
+    <>
+      <div className="flex flex-grow overflow-auto">
+        <TheSidebar />
+        <TheSidebarOverlay />
+
+        <div className="flex-1 overflow-auto">
+          <TheHeader />
+          <TheMain />
+        </div>
+      </div>
+      <TheRegistration />
+    </>
   );
 }
 
