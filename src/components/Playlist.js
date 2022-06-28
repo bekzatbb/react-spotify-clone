53f12096ebd41afeeb71769c67ee2c4b69f08680
Bekzat <bekzat.bagdatuly03@gmail.com>
2022-06-28 17:30:04 +0600

    react

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -4,16 +4,42 @@ import PlaylistButtonPlay from './PlaylistButtonPlay'
 import PlaylistTitle from './PlaylistTitle'
 import PlaylistDescription from './PlaylistDescription'
 
-function Playlist() {
+const menuItems = [
+  {
+    label: 'Add to Your Library',
+  },
+  {
+    label: 'Share',
+    subMenuItems: [
+      {
+        label: 'Copy link to playlist',
+      },
+      {
+        label: 'Embed playlist',
+      },
+    ],
+  },
+  {
+    label: 'About recommendations',
+  },
+  {
+    label: 'Open in Desktop app',
+  },
+];
+
+function Playlist({ classes, coverUrl, title, description }) {
 	return (
-		<a href="/" className="relative p-4 rounded-md bg-[#181818] hover:bg-[#272727] duration-200 group">
+		<a href="/" className={ `relative p-4 rounded-md bg-[#181818] hover:bg-[#272727] duration-200 group ${classes}` }>
       <div className="relative">
-      	<PlaylistCover />
+      	<PlaylistCover url={ coverUrl } />
       	<PlaylistButtonPlay />
       </div>
-      <PlaylistTitle />
-      <PlaylistDescription />
-      <PlaylistContextMenu />
+      <PlaylistTitle title={ title } />
+      <PlaylistDescription description={ description } />
+      <PlaylistContextMenu
+        menuItems={menuItems}
+        classes="absolute top-9 left-9 bg-[#282828] text-[#eaeaea] text-sm divide-y divide-[#3e3e3e] p-1 rounded shadow-xl cursor-default whitespace-nowrap z-10 hidden group-hover:block"
+      />
     </a>
 	)
 }
--- src/components/PlaylistContextMenuItem.js
+++ src/components/PlaylistContextMenuItem.js
@@ -1,11 +1,28 @@
-function PlaylistContextMenuItem({ children: label }) {
+import { ChevronRightIcon } from '@heroicons/react/outline'
+import PlaylistContextMenu from './PlaylistContextMenu'
+
+function PlaylistContextMenuItem({ children: label, subMenuItems }) {
+	if (subMenuItems) {
+		return (
+		<li className="relative">
+		  <button className="w-full p-3 text-left hover:text-white hover:bg-[#3e3e3e] cursor-default flex justify-between items-center peer">
+				{ label } <ChevronRightIcon className="w-4 h-4" />
+		  </button>
+		  <PlaylistContextMenu 
+		  	menuItems={ subMenuItems }
+		  	classes="absolute top-0 left-full bg-[#282828] text-[#eaeaea] text-sm p-1 rounded shadow-xl cursor-default invisible peer-hover:visible hover:visible"
+		  />
+		</li>
+		);
+	}
+
 	return (
 		<li>
-		  <button className="w-full p-3 text-left hover:text-white hover:bg-[#3e3e3e] cursor-default">
+			<button className="w-full p-3 text-left hover:text-white hover:bg-[#3e3e3e] cursor-default">
 				{ label }
-		  </button>
+			</button>
 		</li>
-	)
+	);
 }
 
 export default PlaylistContextMenuItem
